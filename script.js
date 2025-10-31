6e165f5bd2757f803fce0711729da2608a348fd1
ZZSECCIAC <claudio.seccia@vodafone.com>
2025-10-31 15:45:53 +0100

    lesson 2 - functions and arrow functions

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ const renovationJob = {
   },
 };
 
-const error = {
+let error = {
   statusCode: 404,
   description: "...",
   retry: false,
@@ -55,3 +55,62 @@ const error = {
     time: 5,
   },
 };
+
+// using console.log
+// console.log("access individual variable", description);
+// console.log("access array element", floorOptions[1]);
+// console.log("access object element", renovationJob.category);
+// console.log("access whole object", error);
+
+//********************************************** */
+// {
+//   //BLOCK SCOPE
+//   const price = 1000 + 3000;
+//   let tax = 0.15;
+//   var total = price + price * tax; //<---- var is accessible outside the block
+// }
+// console.log(total);
+// -- traditional functions vs arrow functions --
+//traditional function - named function
+// function calculatePrice() {
+//   const price = 1000 + 3000;
+//   console.log(price);
+// }
+// calculatePrice(); //call the function
+
+//adding a parameter
+// function calculatePrice(squareMeters) {
+//   const price = 1000 * squareMeters;
+//   console.log(price);
+// }
+// calculatePrice(5000); //call the function
+
+//returning something from a function
+// function calculatePrice(squareMeters) {
+//   // const price = 1000 * squareMeters;
+//   // return price;
+//   //same as above;
+//   // return 1000 * squareMeters;
+// }
+// const result = calculatePrice(5000); //call the function
+// console.log(result);
+
+//********************************************** */
+//assign name to anonymous function
+// const calculatePrice = function (squareMeters) {
+//   return 1000 * squareMeters;
+// };
+
+// console.log(calculatePrice(5000));
+//********************************************** */
+//arrow function
+// const calculatePrice = (squareMeters) => {
+//   return 1000 + squareMeters;
+// }
+//shorter syntax:
+//same as above (direct return without curly braces for a single operation)
+// const calculatePrice = (squareMeters) => 1000 + squareMeters;
+// console.log(calculatePrice(5000));
+//********************************************** */
+//naming for functions:
+// always start with a verb: ex: calculate, make, destroy, put, delete etc...
