88ac6d1636f72b3c54b423a496f8d7ba54831a1e
ZZSECCIAC <claudio.seccia@vodafone.com>
2025-11-18 13:00:38 +0100

    feat: code review

--- BeginnerJs/BeginnerJsExercise1-Counter/script.js
+++ BeginnerJs/BeginnerJsExercise1-Counter/script.js
@@ -1,53 +1,51 @@
-let counterValue = document.querySelector('.counter__value');
-let counterTitle = document.querySelector('.counter__title');
-const incrementButton = document.querySelector('.counter__button--increase');
-const decrementButton = document.querySelector('.counter__button--decrease');
-const resetButton = document.querySelector('.counter__reset-button')
+let counterValue = document.querySelector(".counter__value");
+let counterTitle = document.querySelector(".counter__title");
+const incrementButton = document.querySelector(".counter__button--increase");
+const decrementButton = document.querySelector(".counter__button--decrease");
+const resetButton = document.querySelector(".counter__reset-button");
 
 function incrementValue() {
-    counterValue.innerHTML++;
+  +counterValue.textContent++;
 }
 function decrementValue() {
-    counterValue.innerHTML--;
+  console.log("counterValue.innerHTML", typeof +counterValue.textContent);
+  +counterValue.textContent--;
 }
 function resetValue() {
-    counterValue.innerHTML = 0;
-    counterValue.style.color = '#1d2507';
-    counterTitle.innerHTML = 'Fancy Counter';
-    incrementButton.disabled = false;
-    decrementButton.disabled = false;
+  counterValue.textContent = 0;
+  counterValue.style.color = "#1d2507";
+  counterTitle.innerHTML = "Fancy Counter";
+  incrementButton.disabled = false;
+  decrementButton.disabled = false;
 }
 
 function changeValueColor() {
-    if (parseInt(counterValue.innerHTML) > 5) {
-        counterValue.style.color = "rgba(255, 0, 0, 0.82)";
-        counterTitle.innerHTML = 'Limit! Buy Pro for > 5';
-        incrementButton.disabled = true;
-        decrementButton.disabled = true;
-    } else if (parseInt(counterValue.innerHTML) > 0) {
-        counterValue.style.color = 'rgba(0, 68, 9, 1)';
-    }
-    else if (parseInt(counterValue.innerHTML) < 0) {
-        counterValue.style.color = "rgba(0, 76, 255, 0.82)";
-        decrementButton.disabled = true;
-    }
-    else {
-        counterValue.style.color = "#1d2507";
-        decrementButton.disabled = false;
-    }
-
+  if (parseInt(counterValue.innerHTML) > 5) {
+    counterValue.style.color = "rgba(255, 0, 0, 0.82)";
+    counterTitle.innerHTML = "Limit! Buy Pro for > 5";
+    incrementButton.disabled = true;
+    decrementButton.disabled = true;
+  } else if (parseInt(counterValue.innerHTML) > 0) {
+    counterValue.style.color = "rgba(0, 68, 9, 1)";
+  } else if (parseInt(counterValue.innerHTML) < 0) {
+    counterValue.style.color = "rgba(0, 76, 255, 0.82)";
+    decrementButton.disabled = true;
+  } else {
+    counterValue.style.color = "#1d2507";
+    decrementButton.disabled = false;
+  }
 }
 
-incrementButton.addEventListener('click', () => {
-    incrementValue();
-    changeValueColor();
+incrementButton.addEventListener("click", () => {
+  incrementValue();
+  changeValueColor();
 });
 
-decrementButton.addEventListener('click', () => {
-    decrementValue();
-    changeValueColor();
+decrementButton.addEventListener("click", () => {
+  decrementValue();
+  changeValueColor();
 });
 
-resetButton.addEventListener('click', () => {
-    resetValue();
+resetButton.addEventListener("click", () => {
+  resetValue();
 });
